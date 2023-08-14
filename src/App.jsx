f533ff39dd0f8e71d3b9347e5878a3f821afe1a0
Baldeep Singh <baldeep.singh@spinny.com>
2023-08-14 13:38:54 +0530

    Added Table and Form Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import { z } from "zod";
+// import { z } from "zod";
 import "./App.css";
 import { DataTable } from "./components/ui/DataTable";
 import Form from "./components/ui/Form";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+import { useGetQuery } from "./lib/apiHooks";
+import { useState } from "react";
 
-const formSchema = z.object({
-  name: z.string().nonempty("Name is required"),
-  email: z.string().email().nonempty("Email is required"),
-  gender: z.string().nonempty("Please select a value"),
-  terms: z.boolean().refine((val) => val === true, {
-    message: "You must agree to the terms and conditions",
-  }),
-  multiText: z.array(z.string()).refine((val) => val.length > 0, {
-    message: "You must enter at least one value",
-  }),
-  radio: z.string().refine((val) => val !== "", {
-    message: "You must select a radio option",
-  }),
-});
+// const formSchema = z.object({
+//   name: z.string().nonempty("Name is required"),
+//   email: z.string().email().nonempty("Email is required"),
+//   gender: z.string().nonempty("Please select a value"),
+//   terms: z.boolean().refine((val) => val === true, {
+//     message: "You must agree to the terms and conditions",
+//   }),
+//   multiText: z.array(z.string()).refine((val) => val.length > 0, {
+//     message: "You must enter at least one value",
+//   }),
+//   radio: z.string().refine((val) => val !== "", {
+//     message: "You must select a radio option",
+//   }),
+// });
 
 // Form Configuration
 const formConfig = [
   {
-    name: "name",
-    label: "Name",
+    name: "username",
+    label: "Username",
     type: "text",
     labelClassname: "text-left",
     placeholder: "Enter your name",
@@ -32,6 +34,13 @@ const formConfig = [
   {
     name: "email",
     label: "Email",
+    type: "text",
+    labelClassname: "text-left",
+    placeholder: "Enter your email",
+  },
+  {
+    name: "website",
+    label: "Website",
     type: "text",
     labelClassname: "text-left",
     placeholder: "Enter your email",
@@ -46,12 +55,6 @@ const formConfig = [
       { label: "Female", value: "female" },
       { label: "Other", value: "other" },
     ],
-  },
-  {
-    name: "terms",
-    label: "Terms and Conditions",
-    type: "checkbox",
-    labelClassname: "text-left",
   },
   {
     name: "radio",
@@ -75,41 +78,74 @@ const formConfig = [
 ];
 
 function App() {
+  const [filterString, setFilterString] = useState("");
   const rhfProps = useForm({
-    resolver: zodResolver(formSchema),
+    // resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
+      username: "",
       email: "",
-      password: "",
+      website: "",
       gender: "",
-      terms: false,
-      checkbox: false,
-      multiText: [""],
       radio: "",
     },
   });
 
+  const { data, isLoading } = useGetQuery(
+    ["users", filterString],
+    `https://jsonplaceholder.typicode.com/users${filterString}`
+  );
+
   const handleSubmit = (data) => {
-    console.log(data);
+    const filterString = Object.entries(data).reduce((acc, [key, value]) => {
+      if (value) {
+        if (Array.isArray(value)) {
+          return [...acc, `${key}=${value.join(",")}`];
+        }
+        return [...acc, `${key}=${value}`];
+      }
+      return acc;
+    }, []);
+    setFilterString(filterString !== "" ? `?${filterString.join("&")}` : "");
   };
   return (
     <div className="flex w-full gap-4">
-      <DataTable
-        columns={[
-          {
-            header: "Name",
-            accessorKey: "name",
-          },
-          {
-            header: "Age",
-            accessorKey: "age",
-          },
-        ]}
-        data={[
-          { name: "John", age: 42 },
-          { name: "Jane", age: 36 },
-        ]}
-      />
+      {isLoading && (
+        <div className="w-full rounded border p-6 text-left">Loading...</div>
+      )}
+      {!isLoading && !data.length && (
+        <div className="w-full rounded border p-6 text-left">No data found</div>
+      )}
+      {!isLoading && data.length && (
+        <DataTable
+          columns={[
+            {
+              header: "id",
+              accessorKey: "id",
+            },
+            {
+              header: "Name",
+              accessorKey: "name",
+            },
+            {
+              header: "Email",
+              accessorKey: "email",
+            },
+            {
+              header: "User Name",
+              accessorKey: "username",
+            },
+            {
+              header: "Phone",
+              accessorKey: "phone",
+            },
+            {
+              header: "Website",
+              accessorKey: "website",
+            },
+          ]}
+          data={data}
+        />
+      )}
       <div className="w-2/5 rounded border p-6 text-left">
         <h1 className="text-xl font-bold mb-4">Filters:</h1>
         <Form
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
+    port: 5174,
     host: true,
     strictPort: true,
     watch: {
